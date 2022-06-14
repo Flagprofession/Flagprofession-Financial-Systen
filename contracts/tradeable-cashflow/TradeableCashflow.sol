92cd1487a2203e7ac9a4093cfab03db268e1b0d0
Flagprofession <anton675990@gmail.com>
2022-06-14 14:43:51 +0800

    merge

---
+++ contracts/tradeable-cashflow/TradeableCashflow.sol
@@ -0,0 +1,21 @@
+
+pragma solidity ^0.8.0;
+
+// import {RedirectAll, ISuperToken, ISuperfluid} from "./RedirectAll.sol";
+ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TradeableCashflow is ERC721 {
+
+  constructor (
+    address owner,
+    string memory _name,
+    string memory _symbol
+    //ISuperfluid host,
+    //ISuperToken acceptedToken
+  )
+    ERC721 ( _name, _symbol )
+      {
+
+      _mint(owner, 1);
+  }
+}
