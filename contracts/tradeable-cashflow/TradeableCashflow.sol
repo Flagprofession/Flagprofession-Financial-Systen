427807a7834f978212d87a1a0e514e1dfc015e63
Flagprofession <101234682+Flagprofession@users.noreply.github.com>
2022-08-11 09:18:46 +0800

    Update TradeableCashflow.sol

--- contracts/tradeable-cashflow/TradeableCashflow.sol
+++ contracts/tradeable-cashflow/TradeableCashflow.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.0;
- import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TradeableCashflow is ERC721 {
 
   constructor (
